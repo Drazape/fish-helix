932c3a9c57f0fe3f2d85c0faad1fb24c1290e821
Drazape <Drazape@outlook.com>
2026-05-18 12:29:06 +0530

    feat: default mode prompt: add variants
    Merge changes from upstream.
    
    # Default
    The default is not changed. Square brackets without dimming indicating the mode
    
    # Dimmed
    To make the mode more noticeable, the square brackets surrounding the letter indicating the mode are dimmed.
    This highlights what actually changes when the mode is switched.
    
    # 3-letter
    Mode without the square-brackets, comprising of 3 letters

--- functions/fish_default_mode_prompt.fish
+++ functions/fish_default_mode_prompt.fish
@@ -1,26 +1,45 @@
-function fish_default_mode_prompt --description "Display vi/helix prompt mode"
+function fish_default_mode_prompt --description "Display vi prompt mode"
     # Do nothing if not in vi mode
-    if test "$fish_key_bindings" = fish_vi_key_bindings
-        or test "$fish_key_bindings" = fish_helix_key_bindings
-        or test "$fish_key_bindings" = fish_hybrid_key_bindings
-        switch $fish_bind_mode
-            case default
-                set_color --bold red
-                echo '[N]'
-            case insert
-                set_color --bold green
-                echo '[I]'
-            case replace_one
-                set_color --bold green
-                echo '[R]'
-            case replace
-                set_color --bold cyan
-                echo '[R]'
-            case visual
-                set_color --bold magenta
-                echo '[V]'
-        end
-        set_color --reset
-        echo -n ' '
+    if not contains -- "$fish_key_bindings" fish_vi_key_bindings fish_hybrid_key_bindings fish_helix_key_bindings
+        return
     end
+    switch $fish_bind_mode
+        case default
+            set -f color red
+            set -f text NOR
+        case insert
+            set -f color green
+            set -f text INS
+        case replace_one
+            set -f color green
+            set -f text RE1
+        case replace
+            set -f color cyan
+            set -f text REP
+        case visual
+            set -f color magenta
+            set -f text VIS
+        case operator f F t T
+            set -f color cyan
+            set -f text NOR
+    end
+
+    set_color --bold $color
+    switch "$fish_mode_prompt_variant"
+        case dim dimmed
+            set_color --dim
+            echo -n [
+
+            set_color --reset --bold $color
+            echo -n (string sub --length=1 -- $text)
+
+            set_color --dim
+            echo ]
+        case triple 3 three
+            echo $text
+        case \*
+            echo [(string sub --length=1 -- $text)]
+    end
+    set_color --reset
+    echo -n ' '
 end
